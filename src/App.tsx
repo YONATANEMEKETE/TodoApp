32294db0677bf893c5a17f842f9869e61618e643
YONATANE MEKETE <yonatanemekete33@gmail.com>
2024-06-04 02:54:36 -0700

    set up everything we need

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-4xl font-header font-bold text-myaccent">
+        Hello world!
+      </h1>
+    </>
+  );
 }
 
 export default App;
